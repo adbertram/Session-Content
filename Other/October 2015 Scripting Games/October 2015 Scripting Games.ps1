4506c8cce44bc0af302f0b811a3f9894a22d45ea
Adam Bertram <adbertram@gmail.com>
2015-09-23 09:38:10 -0500

    updated oct scripting games script

--- Other/October 2015 Scripting Games/October 2015 Scripting Games.ps1
+++ Other/October 2015 Scripting Games/October 2015 Scripting Games.ps1
@@ -1,6 +1,50 @@
-﻿function Get-RssFeed
+﻿#requires -Version 3
+
+function Get-RssFeed
 {
+	<#
+	.SYNOPSIS
+		This function retrieved items from a RSS feed.
+
+	.DESCRIPTION
+		This function attempts to read all items that exist in a RSS feed. If found, it will then output a standard set
+		of attributes about each item that provide a useful representation of the contents of each item.
+
+	.PARAMETER Uri
+		This is the Uri or Url of the feed that you'd like to query. It must be a valid URL.
+
+	.PARAMETER Author
+		Use this parameter if you'd like to limit the items shown by the author name. This parameter will match the creator
+		field in the XML file returned.
+	
+	.PARAMETER Newest
+		Input a number here to specify the newest X number of items returned. This will only display the latest X items by the 
+		PubDate element in the XML file returned. This parameter cannot be used in conjunction with the Oldest parameter.
+	
+	.PARAMETER Oldest
+		Input a number here to specify the oldest X number of items returned. This will only display the oldest X items by the 
+		PubDate element in the XML file returned. This parameter cannot be used in conjunction with the Newest parameter.
+
+	.EXAMPLE
+		Get-RssFeed -Uri 'http://www.adamtheautomator.com/feed'
+	
+		This example will query the Adam, the Automator blog for all RSS items and return them all.
+
+	.EXAMPLE
+		Get-RssFeed -Uri 'http://www.adamtheautomator.com/feed' -Newest 5
+	
+		This example will query the Adam, the Automator blog for and only display the latest 5 items based on published date.
+
+	.INPUTS
+		System.String
+
+	.OUTPUTS
+		System.Management.Automation.PSCustomObject
+	
+	#>
+	
 	[CmdletBinding(DefaultParameterSetName = 'None')]
+	[OutputType([System.Management.Automation.PSCustomObject])]
 	param
 	(
 		[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
