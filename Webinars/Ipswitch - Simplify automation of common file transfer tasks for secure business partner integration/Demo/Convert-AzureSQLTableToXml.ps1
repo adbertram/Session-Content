ac632f8e2ce8d3733dca84cf4a0cef3b212c1ab9
Adam Bertram <adbertram@gmail.com>
2015-12-03 14:14:44 -0600

    slides

---
+++ Webinars/Ipswitch - Simplify automation of common file transfer tasks for secure business partner integration/Demo/Convert-AzureSQLTableToXml.ps1
@@ -0,0 +1,67 @@
+﻿[CmdletBinding()]
+
+param(
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$XmlFilePath,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$ServerInstance,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Database,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$UserName,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Password,
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[string]$Query,
+	
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$XmlConversionScriptFilePath = 'C:\IpswitchDemo\ConvertDataRow-ToXml.ps1' ## Use this script for other purposes as well
+)
+
+Import-Module SQLPS
+
+#region Query the database and gather all rows from the table
+
+$params = @{
+	'Database' = $Database
+	'ServerInstance' = $ServerInstance
+	'Username' = $UserName
+	'Password' = $Password
+	'OutputSqlErrors' = $true
+	'Query' = $Query
+}
+Write-Output "Querying the database [$($Database)] on server instance [$($ServerInstance)]"
+$sqlRows = Invoke-Sqlcmd @params
+
+#endregion
+
+#region Run the conversion script
+## Dot source the conversion script to make the function inside usable
+. $XmlConversionScriptFilePath
+
+## Create the XML file from the Azure SQL table 'Users'  The ObjectType is the object template that all rows in the Azure SQL table are. ie. In this example,
+## I have a table of user information, object type is User.
+Write-Output "Converting [$($sqlRows.Count)] rows into the XML file [$($XmlFilePath)]"
+$xmlFile = $sqlRows | ConvertDataRow-ToXml -ObjectType User -Path $XmlFilePath
+
+## Show an error in the MOVEit debug log if the number of users in the XML doesn't match the database
+if ($sqlRows.Count -ne (([xml](Get-Content -Path $xmlFile.FullName)).Users.User.Count))
+{
+	$Host.SetShouldExit(1)
+}
+else
+{
+	Write-Output 'Conversion successful!'	
+}
--- Webinars/Ipswitch - Simplify automation of common file transfer tasks for secure business partner integration/Install-DemoPrereqs.ps1
+++ Webinars/Ipswitch - Simplify automation of common file transfer tasks for secure business partner integration/Install-DemoPrereqs.ps1
@@ -8,7 +8,7 @@ $files = [ordered]@{
 	'SQLSysClrTypes.msi' = 'http://go.microsoft.com/fwlink/?LinkID=239644&clcid=0x409' # Microsoft® System CLR Types for Microsoft® SQL Server® 2012 (x64)
 	'SharedManagementObjects.msi' = 'http://go.microsoft.com/fwlink/?LinkID=239659&clcid=0x409' # Microsoft® SQL Server® 2012 Shared Management Objects (x64)
 	'PowerShellTools.msi' = 'http://go.microsoft.com/fwlink/?LinkID=239656&clcid=0x409' # Microsoft® Windows PowerShell Extensions for Microsoft® SQL Server® 2012 (x64)
-	'azure-powershell.0.9.8.msi' = 'https://github.com/Azure/azure-powershell/releases/download/v0.9.8-September2015/azure-powershell.0.9.8.msi' # Azure PowerShell module
+	#'azure-powershell.0.9.8.msi' = 'https://github.com/Azure/azure-powershell/releases/download/v0.9.8-September2015/azure-powershell.0.9.8.msi' # Azure PowerShell module
 	'ConvertDataRow-ToXml.ps1' = 'https://raw.githubusercontent.com/adbertram/Random-PowerShell-Work/master/Database-Datasets/ConvertDataRow-ToXml.ps1'
 }
 
@@ -34,27 +34,11 @@ foreach ($file in $files.GetEnumerator())
 
 #region Move the Azure and SQLPS modules to the system-level module path (MOVEit will look inside this when execting the task)
 
-$paths += 'C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement', 'C:\Program Files\Microsoft SQL Server\110\Tools\PowerShell\Modules\SQLPS'
+#$paths += 'C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement', 'C:\Program Files\Microsoft SQL Server\110\Tools\PowerShell\Modules\SQLPS'
+$paths += 'C:\Program Files\Microsoft SQL Server\110\Tools\PowerShell\Modules\SQLPS'
 foreach ($path in $paths)
 {
 	Move-Item -Path	$path -Destination 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules'
 }
-
-#endregion
-
-## Connect to your subscription
-Add-AzureAccount ## The data collection 60 second will be fixed in the Azure PowerShell module 1.0
-
-#region Open firewall rule to server
-## https://azure.microsoft.com/en-us/documentation/articles/sql-database-connect-query/
-
-## Get your Azure SQL server name --I only have a single server here so I'll be using that one.
-$azSqlServer = Get-AzureSqlDatabaseServer
-
-## Get your client's public IP --can always to a block range if you'd like.
-$ipAddress = (Invoke-WebRequest 'http://myexternalip.com/raw').Content -replace "`n"
-
-## Create the firewall rule
-New-AzureSqlDatabaseServerFirewallRule -ServerName $azSqlServer.ServerName -RuleName 'ClientRule' -StartIpAddress $ipAddress -EndIpAddress $ipAddress
 
 #endregion
