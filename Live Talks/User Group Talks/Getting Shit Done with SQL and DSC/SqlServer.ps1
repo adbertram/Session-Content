d3ffaaf2fea5005d87dd0a2f07b69c3dc962ca98
Adam Bertram <adbertram@gmail.com>
2016-11-09 11:08:39 -0600

    update

--- Live Talks/User Group Talks/Getting Shit Done with SQL and DSC/SqlServer.ps1
+++ Live Talks/User Group Talks/Getting Shit Done with SQL and DSC/SqlServer.ps1
@@ -3,65 +3,41 @@
 Configuration SQLStandalone
 {
     param(
-        [pscredential]$SetupCredential
+        [pscredential]$SetupCredential ## Need to pass a credential for setup
     )
+    ## Download the xSQLServer module from the PowerShell Gallery
     Import-DscResource -Module xSQLServer
 
-    Node $AllNodes.NodeName
+    ## Run this DSC configuration on the localhost
+    Node 'localhost'
     {
-        # Set LCM to reboot if needed
-        LocalConfigurationManager
-        {
-            DebugMode = "ForceModuleImport"
-            RebootNodeIfNeeded = $true
-        }
-
+        ## Install a prerequsite Windows feature
         WindowsFeature "NET-Framework-Core"
         {
             Ensure = "Present"
             Name = "NET-Framework-Core"
-            Source = "$($ConfigurationData.NonNodeData.Roles.SqlServer.SourcePath)\WindowsServer2012R2\sources\sxs"
+            Source = "\\MEMBERSRV1\Installers\WindowsServer2012R2\sources\sxs"
         }
-        
-        if ($Node.Role -eq 'SQLServer') {
-            xSqlServerSetup 'SqlServerSetup'
-            {
-                DependsOn = "[WindowsFeature]NET-Framework-Core"
-                SourcePath = "$($ConfigurationData.NonNodeData.Roles.SqlServer.SourcePath)\SqlServer2016"
-                SetupCredential = $SetupCredential
-                InstanceName = $ConfigurationData.NonNodeData.Roles.SqlServer.InstanceName
-                Features = $ConfigurationData.NonNodeData.Roles.SqlServer.Features
-                SQLSysAdminAccounts = $ConfigurationData.NonNodeData.Roles.SqlServer.AdminAccount
-                InstallSharedDir = "C:\Program Files\Microsoft SQL Server"
-                InstallSharedWOWDir = "C:\Program Files (x86)\Microsoft SQL Server"
-                InstanceDir = "C:\Program Files\Microsoft SQL Server"
-                InstallSQLDataDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                SQLUserDBDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                SQLUserDBLogDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                SQLTempDBDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                SQLTempDBLogDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                SQLBackupDir = "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\Data"
-                ASDataDir = "C:\Program Files\Microsoft SQL Server\MSAS11.MSSQLSERVER\OLAP\Data"
-                ASLogDir = "C:\Program Files\Microsoft SQL Server\MSAS11.MSSQLSERVER\OLAP\Log"
-                ASBackupDir = "C:\Program Files\Microsoft SQL Server\MSAS11.MSSQLSERVER\OLAP\Backup"
-                ASTempDir = "C:\Program Files\Microsoft SQL Server\MSAS11.MSSQLSERVER\OLAP\Temp"
-                ASConfigDir = "C:\Program Files\Microsoft SQL Server\MSAS11.MSSQLSERVER\OLAP\Config"
-            }
 
-            xSqlServerFirewall 'SqlFirewall'
-            {
-                DependsOn = '[xSqlServerSetup]SqlServerSetup'
-                SourcePath = "$($ConfigurationData.NonNodeData.Roles.SqlServer.SourcePath)\SqlServer2016"
-                InstanceName = $ConfigurationData.NonNodeData.Roles.SqlServer.InstanceName
-                Features = $ConfigurationData.NonNodeData.Roles.SqlServer.Features
-            }
+        ## Have DSC grab install bits from the SourcePath, install under the instance name with the features
+        ## using the specified SQL Sys admin accounts. Be sure to install the Windows feature first.
+        xSqlServerSetup 'SqlServerSetup'
+        {
+            DependsOn = "[WindowsFeature]NET-Framework-Core"
+            SourcePath = '\\MEMBERSRV1\Installers\SqlServer2016'
+            SetupCredential = $SetupCredential
+            InstanceName = 'MSSQLSERVER'
+            Features = 'SQLENGINE,FULLTEXT,RS,AS,IS'
+            SQLSysAdminAccounts = 'mylab.local\Administrator'
+        }
+
+        ## Add firewall exceptions for SQL Server but run SQL server setup first.
+        xSqlServerFirewall 'SqlFirewall'
+        {
+            DependsOn = '[xSqlServerSetup]SqlServerSetup'
+            SourcePath = '\\MEMBERSRV1\Installers\SqlServer2016'
+            InstanceName = 'MSSQLSERVER'
+            Features = 'SQLENGINE,FULLTEXT,RS,AS,IS'
         }
     }
 }
-
-$InstallerServiceAccount = Get-Credential -UserName 'mylab.local\administrator' -Message 'Input credentials for installing SQL Server'
-#$LocalSystemAccount = Get-Credential "SYSTEM"
-
-SQLStandalone -ConfigurationData "$PSScriptRoot\ConfigurationData.psd1" -SetupCredential $InstallerServiceAccount
-Set-DscLocalConfigurationManager -Path .\SQLStandalone -Verbose
-Start-DscConfiguration -Path .\SQLStandalone -Verbose -Wait -Force
--- Live Talks/User Group Talks/Getting Shit Done with SQL and DSC/demo.ps1
+++ Live Talks/User Group Talks/Getting Shit Done with SQL and DSC/demo.ps1
@@ -1,16 +1,18 @@
 #region Intro to DSC
 
-## Scenario #1 - Ensuring a file is present
+## Scenario #1 - Ensuring a file is present with something in it
 
 ## With PowerShell -- no validation or logging (GIT 'ER DONE!) bad
 $filePath = 'C:\SomeImportFile.txt'
 Add-Content -Path $filePath -Value 'Adam deserve a raise --from manager'
+Get-Content -Path $filepath
+del $filepath
 
 ## With PowerShell --with validation and logging (alot more code) better
 
 if (-not (Test-Path -Path $FilePath -PathType Leaf)) {
     Write-Host -Message "The file [$($filePath)] does not exist yet." 
-    New-Item -Path $filePath -ItemType File
+    $null = New-Item -Path $filePath -ItemType File
 }
 
 Add-Content -Path $filePath -Value 'Adam deserve a raise --from manager'
@@ -21,9 +23,15 @@ if ((Get-Content -Path $filePath -Raw) -match 'Adam deserve a raise --from manag
     Write-Host 'Oh noes! The file could not be created.' -ForegroundColor Red
 }
 
-## Run the same code again
+## Run the same code again --PowerShell is dumb
+Get-Content -Path $filePath
 
 ## With DSC
+
+## Use the built-in File resource
+Get-DscResource File -Syntax
+
+#region Create the configuration
 configuration CreateTheFile {
 
     $filePath = 'C:\SomeImportFile.txt'
@@ -35,26 +43,63 @@ configuration CreateTheFile {
         }
     }
 }
+#endregion
 
-## Execute the configuration to create the node's MOF file
+#region Execute the configuration to create the node's MOF file
 CreateTheFile
+ls '.\CreateTheFile'
+gc .\CreateTheFile\SQLSRV.mylab.local.mof
+#endregion
 
-## "CreateTheFile" creates a MOF file for the node SQLSRV.mylab.local
-ls 'C:\SQLDSC\CreateTheFile'
-
-## Invoke DSC to consume the MOF file which takes over --this creates the file
+## Invoke DSC to consume the MOF file which takes over --this ensures the file is there and contents
+## no change. looks good
 Start-DscConfiguration -Path .\CreateTheFile -Verbose -Wait -Force
+
+## change the contents a little
+Set-Content -Path $filePath -Value 'whatever' -PassThru
+gc $filepath
 
 ## Run again and it does nothing because the file is already there
 Start-DscConfiguration -Path .\CreateTheFile -Verbose -Wait -Force
 
-## Delete the file, run again and the file is back
-del C:\SomeImportFile.txt
-Start-DscConfiguration -Path .\CreateTheFile -Verbose -Wait -Force
+## Check again
+gc $filePath
 
 #endregion
 
+#region SQL
 
-## Download the required DSC module
 
-## Run the configuration
+## no SQL server resource by default
+Get-DscResource
+
+## find one from the PowerShell Gallery
+$resource = Find-Module xSqlServer
+$resource
+
+## install it
+$resource | Install-Module
+
+$InstallerServiceAccount = Get-Credential -UserName 'mylab.local\administrator' -Message 'Input credentials for installing SQL Server'
+
+## Make the confiugration available
+. .\SqlServer.ps1
+
+## Run the configuration which creates the MOF
+SQLStandalone -ConfigurationData "$PSScriptRoot\ConfigurationData.psd1" -SetupCredential $InstallerServiceAccount
+
+## The configuration was created from combining "configuration data" with the actual configuration. This allows you to
+## separate out per-server values from the configuration to share configurations.
+
+## DSC takes over
+Start-DscConfiguration -Path .\SQLStandalone -Verbose -Wait -Force
+
+#endregion
+
+### BENEFITS OF DSC ###
+<#
+1. No if/then logic in code. Just state what you want and DSC does the rest.
+2. A lot less code all together.
+3. No need to remember complicated install switches.
+4. Leverage lots of existing resources via PowerShell Gallery
+#>
