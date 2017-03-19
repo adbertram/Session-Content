eb95b2e6061934571214aba9831f5d268ecdf74e
Adam Bertram <adbertram@gmail.com>
2017-03-19 08:42:21 -0500

    Added Summit talk updates

--- Live Talks/PowerShell Summit 2017/Infrastructure Testing with Pester/Demos/Project/New-TestEnvironment.Tests.ps1
+++ Live Talks/PowerShell Summit 2017/Infrastructure Testing with Pester/Demos/Project/New-TestEnvironment.Tests.ps1
@@ -39,168 +39,205 @@ param(
     [string]$DependencyFailureAction = 'Exit'
 )
 
-describe 'TestEnvironment' {
-
-    $expectedDomainControllerName = @($expectedAttributes.AllNodes[0]).where({ $_.Purpose -eq 'Domain Controller' -and $_.NodeName -ne '*' }).Nodename
-    $expectedVmName = $expectedDomainControllerName
-    $hyperVSrv = 'HYPERVSRV'
-    
-    if ($Full.IsPresent) {
-
-        #region Dependency checking and remediation
-            $hyperVCred = (Import-Clixml -Path "$PSScriptRoot\HyperVCredential.xml")
-
-            ## Check all dependencies. These are ordered for a reason.
-            $dependencies = [ordered]@{
-                'VM is created' = @{
-                    CheckAction = {
-                        $vmCheck = { 
-                            if (Get-VM -Name $using:expectedVmName -ErrorAction 'SilentlyContinue') {
-                                $true
-                            }  else {
-                                $false
-                            }
-                        }
-                        Invoke-Command -ComputerName $hyperVSrv -ScriptBlock $vmCheck 
-                    }
-                    RemediationAction = {
-                        throw 'VM creation remediation code not yet implemented. Laziness in full effect.'
-                    }
-                    RemoveAction = {
-                        throw 'VM creation removal code not yet implemented. Laziness in full effect.'
-                    }
-                }
-                'VM is running' = @{
-                    CheckAction = {
-                        $vmCheck = { 
-                            if (Get-VM -Name $using:expectedVmName -ErrorAction 'SilentlyContinue' | where { $_.State -eq 'Running' }) {
-                                $true
-                            }  else {
-                                $false
-                            }
-                        }
-                        Invoke-Command -ComputerName $hyperVSrv -ScriptBlock $vmCheck 
-                    }
-                    RemediationAction = {
-                        Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-VM -Name $using:expectedVmName | Start-Vm }
-                    }
-                    RemoveAction = {
-                        Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-VM -Name $using:expectedVmName | Stop-Vm }
-                    }
-                }
-                'VM can be found by name' = @{
-                    CheckAction = {
-                        (Resolve-DnsName -Name $expectedDomainControllerName -ErrorAction 'SilentlyContinue') -ne $null
-                    }
-                    RemediationAction = {
-                        Write-Warning -Message 'No specific remediation necessary for DNS problem. You have probably got bigger problems.'
-                    }
-                    RemoveAction = {
-                        Write-Verbose -Message 'No specific remove action necessary for DNS problem.'
-                    }  
-                }
-                'VM is pingable' = @{
-                    CheckAction = {
-                        Test-Connection -Name $expectedDomainControllerName -Quiet -Count 1
-                    }
-                    RemediationAction = {
-                        throw 'VM is running but it is not pingable. This is where I might do something to the VM firewall.'
-                    }
-                    RemoveAction = {
-                        Write-Verbose -Message 'No specific remove action necessary for unpingable (is that a word?) VM.'
-                    }
-                    
-                }
-                'VM has PS Remoting available' = @{
-                    CheckAction = {
-                        (Invoke-Command  -ComputerName $expectedDomainControllerName -ScriptBlock {1}) -eq 1
-                    }
-                    RemediationAction = {
-                        throw 'VM cannot be remoted to. This is where I might try to enable WinRM.'
-                    }
-                    RemoveAction = {
-                        Write-Verbose -Message 'No specific remove action necessary for unresponsive PS remoting VM.'
-                    }
-                }
-            }
-
-            $script:removeActions = @()
-            foreach ($dep in $dependencies.GetEnumerator()) {
-                if (-not (& $dep.Value.CheckAction)) {
-                    if ($DependencyFailureAction -eq 'Exit') {
-                        throw "The dependency [$($dep.Key)] failed. Halting tests.."
-                    } else {
-                        Write-Verbose -Message "The dependency [$($dep.Key)] failed but hold on. I am remediating this..."
-                        $script:removeActions += $dep.Value.RemoveAction
-                        & $dep.Value.RemediationAction
-                    }
-                }
-            }
-        #endregion
-
-        AfterAll {
-            ## Tear down any dependencies created and all changes made to environment
-            Write-Verbose -Message 'Cleaning up the mess...'
-            & "$PSScriptRoot\Remove-TestEnvironment.ps1"
-
-            foreach ($removeAction in $script:removeActions) {
-                & $removeAction
-            }
-        }
+try {
+    $whParams = @{
+        BackgroundColor = 'Black'
     }
 
-    ## Execute the code we're testing
-    & "$PSScriptRoot\New-TestEnvironment.ps1"
-
-    ## Run all tests
     ## Read the expected attributes from ConfigurationData
     $expectedAttributes = Import-PowerShellDataFile -Path "$PSScriptRoot\ConfigurationData.psd1"
-    
-    
-    $sharedSession = New-PSSession -ComputerName $expectedDomainControllerName
+        
+    $expectedDomainControllerName = @($expectedAttributes.AllNodes).where({ $_.Purpose -eq 'Domain Controller' -and $_.NodeName -ne '*' }).Nodename
+    $expectedVmName = $expectedDomainControllerName
 
-    ## Forest-wide
-    $forest = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdForest }
-    
-    ## Groups
-    $actualGroups = Invoke-Command -Session $sharedSession -ScriptBlock {  Get-AdGroup -Filter '*' } | Select -ExpandProperty Name
-    $expectedGroups = $expectedAttributes.NonNodeData.AdGroups
-    
-    ## OUs
-    $actualOuDns = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdOrganizationalUnit -Filter '*' } | Select -ExpandProperty DistinguishedName
-    $expectedOus = $expectedAttributes.NonNodeData.OrganizationalUnits
-    $expectedOuDns = $expectedOus | foreach { "OU=$_,DC=mylab,DC=local" }
+    if ($Full.IsPresent)
+    {
+        #region Dependency checking and remediation
+        $hyperVCred = (Import-Clixml -Path "$PSScriptRoot\HyperVCredential.xml")
+        $hyperVSrv = 'HYPERVSRV'
 
-    ## Users
-    $actualUsers = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdUser -Filter "*" -Properties Department,Title }
-    $expectedUsers = $expectedAttributes.NonNodeData.AdUsers
+        ## Check all dependencies. These are ordered for a reason.
+        $dependencies = [ordered]@{
+            'VM is created' = @{
+                CheckAction = {
+                    $vmCheck = { 
+                        if (Get-VM -Name $using:expectedVmName -ErrorAction 'SilentlyContinue')
+                        {
+                            $true
+                        }  else
+                        {
+                            $false
+                        }
+                    }
+                    Invoke-Command -ComputerName $hyperVSrv -ScriptBlock $vmCheck 
+                }
+                RemediationAction = {
+                    throw 'VM creation remediation code not yet implemented. Laziness in full effect.'
+                }
+                RemoveAction = {
+                    Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-VM -Name $using:expectedVmName | Get-VMSnapshot | Restore-VmSnapshot }
+                }
+            }
+            'VM is running' = @{
+                CheckAction = {
+                    $vmCheck = { 
+                        if (Get-VM -Name $using:expectedVmName -ErrorAction 'SilentlyContinue' | where { $_.State -eq 'Running' })
+                        {
+                            $true
+                        }  else
+                        {
+                            $false
+                        }
+                    }
+                    Invoke-Command -ComputerName $hyperVSrv -ScriptBlock $vmCheck 
+                }
+                RemediationAction = {
+                    Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-VM -Name $using:expectedVmName | Start-Vm }
+                    ## Wait for it to boot
+                    while (-not (Test-Connection -ComputerName $expectedVmName -Quiet -Count 1)) {
+                        Start-Sleep -Seconds 5
+                    }
+                }
+                RemoveAction = {
+                    Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-VM -Name $using:expectedVmName | Stop-Vm }
+                }
+            }
+            'VM can be found by name' = @{
+                CheckAction = {
+                    (Resolve-DnsName -Name $expectedDomainControllerName -ErrorAction 'SilentlyContinue') -ne $null
+                }
+                RemediationAction = {
+                    Write-Warning -Message 'No specific remediation necessary for DNS problem. You have probably got bigger problems.'
+                }
+                RemoveAction = {
+                    Write-Verbose -Message 'No specific remove action necessary for DNS problem.'
+                }  
+            }
+            'VM is pingable' = @{
+                CheckAction = {
+                    Test-Connection -ComputerName $expectedDomainControllerName -Quiet -Count 1
+                }
+                RemediationAction = {
+                    throw 'VM is running but it is not pingable. This is where I might do something to the VM firewall.'
+                }
+                RemoveAction = {
+                    Write-Verbose -Message 'No specific remove action necessary for unpingable (is that a word?) VM.'
+                }
+                    
+            }
+            'VM has PS Remoting available' = @{
+                CheckAction = {
+                    (Invoke-Command  -ComputerName $expectedDomainControllerName -ScriptBlock {1}) -eq 1
+                }
+                RemediationAction = {
+                    throw 'VM cannot be remoted to. This is where I might try to enable WinRM.'
+                }
+                RemoveAction = {
+                    Write-Verbose -Message 'No specific remove action necessary for unresponsive PS remoting VM.'
+                }
+            }
+        }
 
-    it "creates the expected forest" {
-        $forest.Name | should be $expectedAttributes.NonNodeData.DomainName
+
+        Write-Host @whParams -Object "====Start Dependency Check====" -ForegroundColor Magenta
+        $script:removeActions = @()
+        foreach ($dep in $dependencies.GetEnumerator())
+        {
+            Write-Host @whParams -Object "-Checking dependency [$($dep.Key)]..." -ForegroundColor Yellow
+            if (-not (& $dep.Value.CheckAction))
+            {
+                if ($DependencyFailureAction -eq 'Exit')
+                {
+                    throw "The dependency [$($dep.Key)] failed. Halting tests.."
+                } else
+                {
+                    Write-Host @whParams -Object "---The dependency [$($dep.Key)] failed but hold on. I am remediating this..." -ForegroundColor Cyan
+                    $script:removeActions += $dep.Value.RemoveAction
+                    & $dep.Value.RemediationAction
+                }
+            } 
+            else
+            {
+                Write-Host @whParams -Object "---The dependency [$($dep.Key)] passed." -ForegroundColor Green
+            }
+        }
+        Write-Host @whParams -Object "====End Dependency Check====" -ForegroundColor Magenta
+        #endregion
     }
 
-    it 'creates all expected AD Groups' {
+    $domainDn = ('DC={0},DC={1}' -f ($expectedAttributes.NonNodeData.DomainName -split '\.')[0], ($expectedAttributes.NonNodeData.DomainName -split '\.')[1])
 
-    @($actualGroups | where { $_ -in $expectedGroups }).Count | should be @($expectedGroups).Count
+    Write-Host @whParams -Object "====Begin Code Execution====" -ForegroundColor Magenta
+    & "$PSScriptRoot\New-TestEnvironment.ps1"
+    Write-Host @whParams -Object "====End Code Execution====" -ForegroundColor Magenta
 
-    }
+    describe 'New-TestEnvironment' {
 
-    it 'creates all expected AD OUs' {
+        ## Run all tests
+        $sharedSession = New-PSSession -ComputerName $expectedDomainControllerName
+
+        ## Forest-wide
+        $forest = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdForest }
         
-        @($actualOuDns | where { $_ -in $expectedOuDns }).Count | should be @($expectedOuDns).Count
+        ## Groups
+        $actualGroups = Invoke-Command -Session $sharedSession -ScriptBlock {  Get-AdGroup -Filter '*' } | Select -ExpandProperty Name
+        $expectedGroups = $expectedAttributes.NonNodeData.AdGroups
         
-    }
+        ## OUs
+        $actualOuDns = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdOrganizationalUnit -Filter '*' } | Select -ExpandProperty DistinguishedName
+        $expectedOus = $expectedAttributes.NonNodeData.OrganizationalUnits
+        $expectedOuDns = $expectedOus | foreach { "OU=$_,$domainDn" }
 
-    it 'creates all expected AD users' {
-        
-        foreach ($user in $expectedUsers) {
-            $expectedUserName = ('{0}{1}' -f $user.FirstName.SubString(0,1),$user.LastName)
-            $actualUserMatch = $actualUsers | where {$_.SamAccountName -eq $expectedUserName}
-            $actualUserMatch | should not benullorempty     
-            $actualUserMatch.givenName | should be $user.FirstName
-            $actualUserMatch.surName | should be $user.LastName
-            $actualUserMatch.Department | should be $user.Department
-            $actualUserMatch.DistinguishedName | should be "CN=$expectedUserName,OU=$($user.Department),DC=mylab,DC=local"
+        ## Users
+        $actualUsers = Invoke-Command -Session $sharedSession -ScriptBlock { Get-AdUser -Filter "*" -Properties Department, Title }
+        $expectedUsers = $expectedAttributes.NonNodeData.AdUsers
+
+        it "creates the expected forest" {
+            $forest.Name | should be $expectedAttributes.NonNodeData.DomainName
+        }
+
+        it 'creates all expected AD Groups' {
+
+            @($actualGroups | where { $_ -in $expectedGroups }).Count | should be @($expectedGroups).Count
+
+        }
+
+        it 'creates all expected AD OUs' {
+
+            @($actualOuDns | where { $_ -in $expectedOuDns }).Count | should be @($expectedOuDns).Count
+            
+        }
+
+        it 'creates all expected AD users' {
+            
+            foreach ($user in $expectedUsers)
+            {
+                $expectedUserName = ('{0}{1}' -f $user.FirstName.SubString(0, 1), $user.LastName)
+                $actualUserMatch = $actualUsers | where {$_.SamAccountName -eq $expectedUserName}
+                $actualUserMatch | should not benullorempty     
+                $actualUserMatch.givenName | should be $user.FirstName
+                $actualUserMatch.surName | should be $user.LastName
+                $actualUserMatch.Department | should be $user.Department
+                $actualUserMatch.DistinguishedName | should be "CN=$expectedUserName,OU=$($user.Department),$domainDn"
+            }
+        }
+
+        AfterAll {
+            if (@($script:removeActions).Count -eq 0)
+            {
+                Write-Host @whParams -Object 'No dependencies built thus no teardown necessary' -ForegroundColor Yellow
+            } else
+            {
+                Write-Host @whParams -Object "====Begin Teardown====" -ForegroundColor Magenta
+                foreach ($removeAction in $script:removeActions)
+                {
+                    Write-Host @whParams -Object 'Starting remove action...' -ForegroundColor Yellow
+                    & $removeAction
+                }
+                Write-Host @whParams -Object "====End Teardown====" -ForegroundColor Magenta    
+            }
         }
     }
+} catch {
+    Write-Host @whParams -Object $_.Exception.Message -ForegroundColor Red
 }
--- Live Talks/PowerShell Summit 2017/Infrastructure Testing with Pester/Demos/Project/New-TestEnvironment.ps1
+++ Live Talks/PowerShell Summit 2017/Infrastructure Testing with Pester/Demos/Project/New-TestEnvironment.ps1
@@ -1,6 +1,6 @@
 configuration NewTestEnvironment
 {        
-    Import-DscResource -ModuleName xActiveDirectory             
+    Import-DscResource -ModuleName xActiveDirectory        
             
     Node @($AllNodes).where({ $_.Purpose -eq 'Domain Controller' }).Nodename             
     {             
@@ -11,53 +11,53 @@ configuration NewTestEnvironment
             RebootNodeIfNeeded = $true            
         }
 
-        @($ConfigurationData.NonNodeData.ADGroups).foreach({
-            xADGroup $_
-            {
-                Ensure = 'Present'
-                GroupName = $_
-                DependsOn = '[xADDomain]ADDomain'
-            }
-        })
+        @($ConfigurationData.NonNodeData.ADGroups).foreach( {
+                xADGroup $_
+                {
+                    Ensure = 'Present'
+                    GroupName = $_
+                    DependsOn = '[xADDomain]ADDomain'
+                }
+            })
 
-        @($ConfigurationData.NonNodeData.OrganizationalUnits).foreach({
-            xADOrganizationalUnit $_
-            {
-                Ensure = 'Present'
-                Name = ($_ -replace '-')
-                Path = ('DC={1},DC={1}' -f ($Configuration.NonNodeData.DomainName -split '.')[0],($Configuration.NonNodeData.DomainName -split '.')[1])
-                DependsOn = '[xADDomain]ADDomain'
-            }
-        })
+        @($ConfigurationData.NonNodeData.OrganizationalUnits).foreach( {
+                xADOrganizationalUnit $_
+                {
+                    Ensure = 'Present'
+                    Name = ($_ -replace '-')
+                    Path = ('DC={0},DC={1}' -f ($ConfigurationData.NonNodeData.DomainName -split '\.')[0], ($ConfigurationData.NonNodeData.DomainName -split '\.')[1])
+                    DependsOn = '[xADDomain]ADDomain'
+                }
+            })
 
         $pw = ConvertTo-SecureString 'P@$$w0rd19' -AsPlainText -Force
-        $defaultUserCred = New-Object System.Management.Automation.PSCredential ('administrator',$pw)
-        @($ConfigurationData.NonNodeData.ADUsers).foreach({
-            xADUser "$($_.FirstName) $($_.LastName)"
-            {
-                Ensure = 'Present'
-                DomainName = $ConfigurationData.NonNodeData.DomainName
-                GivenName = $_.FirstName
-                SurName = $_.LastName
-                UserName = ('{0}{1}' -f $_.FirstName.SubString(0,1),$_.LastName)
-                Department = $_.Department
-                Path = ("OU={0},DC={1},DC={2}" -f $_.Department,($Configuration.NonNodeData.DomainName -split '.')[0],($Configuration.NonNodeData.DomainName -split '.')[1])
-                JobTitle = $_.Title
-                Password = $defaultUserCred
-                DependsOn = '[xADDomain]ADDomain'
-            }
-        })
+        $defaultUserCred = New-Object System.Management.Automation.PSCredential ('administrator', $pw)
+        @($ConfigurationData.NonNodeData.ADUsers).foreach( {
+                xADUser "$($_.FirstName) $($_.LastName)"
+                {
+                    Ensure = 'Present'
+                    DomainName = $ConfigurationData.NonNodeData.DomainName
+                    GivenName = $_.FirstName
+                    SurName = $_.LastName
+                    UserName = ('{0}{1}' -f $_.FirstName.SubString(0, 1), $_.LastName)
+                    Department = $_.Department
+                    Path = ("OU={0},DC={1},DC={2}" -f $_.Department, ($ConfigurationData.NonNodeData.DomainName -split '\.')[0], ($ConfigurationData.NonNodeData.DomainName -split '\.')[1])
+                    JobTitle = $_.Title
+                    Password = $defaultUserCred
+                    DependsOn = '[xADDomain]ADDomain'
+                }
+            })
 
-        ($Node.WindowsFeatures).foreach({
-            WindowsFeature $_
-            {
-                Ensure = 'Present'
-                Name = $_
-            }
-        })        
+        ($Node.WindowsFeatures).foreach( {
+                WindowsFeature $_
+                {
+                    Ensure = 'Present'
+                    Name = $_
+                }
+            })        
         
         $pw = ConvertTo-SecureString 'p@$$w0rd19' -AsPlainText -Force
-        $domainCred = New-Object System.Management.Automation.PSCredential ('administrator',$pw)
+        $domainCred = New-Object System.Management.Automation.PSCredential ('administrator', $pw)
         xADDomain ADDomain          
         {             
             DomainName = $ConfigurationData.NonNodeData.DomainName
@@ -68,6 +68,8 @@ configuration NewTestEnvironment
     }         
 } 
 
-$null = NewTestEnvironment -ConfigurationData "$PSScriptRoot\ConfigurationData.psd1"
-Set-DSCLocalConfigurationManager -Path .\NewTestEnvironment -Force -ComputerName TESTLABDC -Verbose
-Start-DscConfiguration -Wait -Force -Path .\NewTestEnvironment -ComputerName TESTLABDC -Verbose
+
+Set-Location 'C:\Dropbox\GitRepos\Session-Content\Live Talks\PowerShell Summit 2017\Infrastructure Testing with Pester\Demos\Project'
+$null = NewTestEnvironment -ConfigurationData "$PSScriptRoot\ConfigurationData.psd1" -WarningAction SilentlyContinue
+Set-DSCLocalConfigurationManager -Path .\NewTestEnvironment -ComputerName TESTLABDC
+Start-DscConfiguration -Wait -Force -Path .\NewTestEnvironment -ComputerName TESTLABDC
