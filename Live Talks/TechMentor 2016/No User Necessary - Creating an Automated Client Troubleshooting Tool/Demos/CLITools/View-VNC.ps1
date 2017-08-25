5083aab8e490eee063578ab6405a81f7173008bb
Adam Bertram <adbertram@gmail.com>
2017-08-25 14:55:01 -0500

    update

--- Live Talks/TechMentor 2016/No User Necessary - Creating an Automated Client Troubleshooting Tool/Demos/CLITools/View-VNC.ps1
+++
@@ -1,2 +0,0 @@
-﻿## Statically use CLIENT1 just for testing. We'll fill that in with a variable later.
-start -FilePath "\\MEMBERSRV1\ToolShare\vncviewer.exe" -Args "-connect CLIENT1"
--- Live Talks/Youtube Live/Infrastructure Testing with Pester/Demos/_LeadDemo.ps1
+++ Live Talks/Youtube Live/Infrastructure Testing with Pester/Demos/_LeadDemo.ps1
@@ -1,4 +1,5 @@
 $demoPath = 'C:\Dropbox\GitRepos\Session-Content\Live Talks\Youtube Live\Infrastructure Testing with Pester\Demos'
+cls
 
 ## DEMO 1 - Infrastructure Testing 101
 psedit "$demoPath\Introduction\Infrastructure Testing 101.ps1"
@@ -6,13 +7,13 @@ psedit "$demoPath\Introduction\Infrastructure Testing 101.ps1"
 ## DEMO 2 - Reviewing the DSC configuration and making it happen
 psedit "$demoPath\Project\ConfigurationData.psd1"
 psedit "$demoPath\Project\New-TestEnvironment.ps1"
-& "$demoPath\Project\New-TestEnvironment.ps1"
+cls; & "$demoPath\Project\New-TestEnvironment.ps1"
 
-## DEMO 3 - Dealing with Dependencies
-psedit "$demoPath\Introduction\Dealing with Dependencies.ps1"
-
-## DEMO 4 - Defining Expected Behavior
+## DEMO 3 - Defining Expected Behavior
 psedit "$demoPath\Introduction\Defining Expected Behavior.ps1"
+
+## DEMO 4 - Dealing with Dependencies
+psedit "$demoPath\Introduction\Dealing with Dependencies.ps1"
 
 ## DEMO 5 - Creating the Tests
 psedit "$demoPath\Introduction\Creating Pester Tests.ps1"
@@ -29,30 +30,30 @@ $invPesterParams = @{
 
 ################### All tests passing ##########################
 ## Invoke default set of tests
-Invoke-Pester -Script $invPesterParams -Verbose
+cls; Invoke-Pester -Script $invPesterParams -Verbose
 
 ## Invoke the full test suite of tests but just exit if a dependency was not met
-Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true } }) -Verbose
+cls; Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true } }) -Verbose
 
 ## Invoke the full test suite AND build all the dependencies
-Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true; DependencyFailureAction = 'Build' } }) -Verbose
+cls; Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true; DependencyFailureAction = 'Build' } }) -Verbose
 
 ################### Dependency failure ##########################
 
 ## Stop the VM just to fail the dependency check
 $hyperVCred = (Import-Clixml -Path "$demoPath\Project\HyperVCredential.xml")
 $hyperVSrv = 'HYPERVSRV'
-Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-Vm -Name 'TESTLABDC' | Stop-Vm -Force }
+cls; Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-Vm -Name 'TESTLABDC' | Stop-Vm -Force }
 
 ## Invoke default set of tests --this will fail miserably due to no dep checks
 ## We're not doing this now because it just hangs. It has no idea of it's own dependencies.
 # Invoke-Pester -Script $invPesterParams -Verbose
 
 ## Invoke the full test suite of tests but just exit if a dependency was not met --this will see a dep is missing and fail
-Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true } }) -Verbose
+cls; Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true } }) -Verbose
 
 ## Invoke the full test suite AND build all the dependencies --this will notice the missing dep and correct it
-Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true; DependencyFailureAction = 'Build' } }) -Verbose
+cls; Invoke-Pester -Script ($invPesterParams + @{ Parameters = @{ Full = $true; DependencyFailureAction = 'Build' } }) -Verbose
 
 ## The VM should now have been tested and is back in the state we left it.
-Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-Vm -Name 'TESTLABDC' }
+cls; Invoke-Command -ComputerName $hyperVSrv -ScriptBlock { Get-Vm -Name 'TESTLABDC' }
