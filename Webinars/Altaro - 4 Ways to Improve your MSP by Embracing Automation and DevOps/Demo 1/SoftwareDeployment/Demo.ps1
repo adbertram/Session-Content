9c16423a2d259c53c2860469694758417d5bf93f
Adam Bertram <adbertram@gmail.com>
2018-03-05 08:40:31 -0600

    updates

--- Webinars/Altaro - 4 Ways to Improve your MSP by Embracing Automation and DevOps/Demo 1/SoftwareDeployment/Demo.ps1
+++
@@ -1,7 +0,0 @@
-## Deploy a new zero-day KB patch to multiple computers
-
-$credential = Get-Credential
-
-& "$PSScriptRoot\Deploy-WindowsPatch.ps1" -ComputerName 'CLIENTSERVER1', 'CLIENTSERVER2' -Credential $credential
-
-& "$PSScriptRoot\Deploy-VNC.ps1" -ComputerName 'CLIENTSERVER1', 'CLIENTSERVER2' -Credential $credential
